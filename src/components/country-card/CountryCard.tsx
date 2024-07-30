d739c659aabbe6b43369b63d6fdc3fc461cc0472
Adam Robson <adamrayrobson@gmail.com>
2024-07-29 17:53:18 -0700

    add image to country card component

--- src/components/country-card/CountryCard.tsx
+++ src/components/country-card/CountryCard.tsx
@@ -4,6 +4,7 @@ export default function CountryCard({ name }: { name: string }) {
   return (
     <div className="country-card">
       <span className="country-name">{name}</span>
+      <img className="country-image" src={`https://flagcdn.com/w320/${name.toLowerCase()}.png`} alt={name} height="100" />
     </div>
   )
 }
