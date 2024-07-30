6b5b3bba0c91f78ddfb8041e9806ebaa0f908598
Adam Robson <adamrayrobson@gmail.com>
2024-07-29 17:32:13 -0700

    add component functions into files with default structure

--- src/services/client.tsx
+++ src/services/client.tsx
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_APP_SUPABASE_url;
+const url = import.meta.env.VITE_APP_SUPABASE_URL;
 const key = import.meta.env.VITE_APP_SUPABASE_KEY;
 
-export const client = createClient(url, key);
+const client = createClient(url, key);
+
+export default client;
--- src/services/countries.tsx
+++ src/services/countries.tsx
@@ -1,19 +1,17 @@
-import { client } from './client';
+import client from './client';
 
-export async function fetchAllCountries() {
+export default async function fetchCountries() {
   try {
-    const response = await client
+    const res = await client
       .from('countries')
-      .select('*')
-      .order('name');
+      .select('*');
 
-    return response.data;
+    return res.data;
   } catch (error) {
     if (error instanceof Error) {
       console.error(`There was an error while retrieving the data, in the catch block: ${error.message}`);
     } else {
       console.error(`There was an error while retrieving the data, that is not an "Error": ${error}`);
     }
-    return null;
   }
 }
