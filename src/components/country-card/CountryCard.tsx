e76403c8abfb1255fc54dac4b94f518f359dc6c3
Adam Robson <adamrayrobson@gmail.com>
2024-07-29 19:49:04 -0700

    update country card url and loader path

--- src/components/country-card/CountryCard.tsx
+++ src/components/country-card/CountryCard.tsx
@@ -4,7 +4,11 @@ export default function CountryCard({ name, code }: { name: string, code: string
   return (
     <div className="country-card">
       <span className="country-name">{name}</span>
-      <img className="country-image" src={`https://flagcdn.com/w320/${code.toLowerCase()}.png`} alt={name} height="100" />
+      <img
+        src={`https://flagcdn.com/16x12/${code.toLowerCase()}.png`}
+        width="60"
+        height="45"
+        alt={`${name}`} /> 
     </div>
   )
 }
