21734e7e37b211c7f9d86f8a056052935895f7cc
Adam Robson <adamrayrobson@gmail.com>
2024-07-29 18:29:51 -0700

    add default strings to client file when importing env vars

--- src/services/client.tsx
+++ src/services/client.tsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_APP_SUPABASE_URL;
-const key = import.meta.env.VITE_APP_SUPABASE_KEY;
+const url = import.meta.env.VITE_APP_SUPABASE_URL || '';
+const key = import.meta.env.VITE_APP_SUPABASE_KEY || '';
 
 const client = createClient(url, key);
 
